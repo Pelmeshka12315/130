4a0c7d27337adb7b29747fbe0749a61d6e4e9893
User <https://github.com/DanilRossov/AutoRossov.git>
2023-04-10 08:35:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a130/adapter/DataSender.java
@@ -0,0 +1,10 @@
+package com.example.a130.adapter;
+
+import com.example.a130.NewPost;
+
+import java.util.List;
+
+public interface DataSender {
+
+    public void onDataRecived(List<NewPost> listData);
+}
